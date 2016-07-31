be368109ccdac15f87a64ce49f91cb4816a505e9
demonmx <demon@demon.com.mx>
2016-07-31 00:59:18 -0500

    Create firewallcitos.py

---
+++ firewallcitos.py
@@ -0,0 +1,133 @@
+#!/bin/python
+#
+# this code extract the configuration of all context's on an asa box
+# stores the config as <ip>_<context>.conf 
+# it uses a table called "firewallcitos" (yea i know, dumb list name) where you define the ip's of the 
+# physical asa boxes.
+#
+# you need to define the account & mypass variables
+# its assumed that every box uses the same account and pass and the "en" pass is the same as mypass
+#
+# you can modify the "expect" script stating the relevant "expect" & "send" pairs on add_expect_pairs function
+# (check the main subroutine you will get it if analize it.)
+#
+# This script was created (and executed) on os x and you must be able to run it on any modern linux/osx box
+# for windows... grow up and install linux .. 
+#
+# Adrian de los Santos
+# demon@demon.com.mx
+
+import pexpect
+import sys
+import re
+
+global commands_list, account, mypass
+commands_list=[]
+
+account="myaccount"
+mypass="mypass"
+
+def add_expect_pairs(expect_part, send_part):
+	global commands_list
+	command_dict = { 'type' : 'expect', 'command' : expect_part }
+	commands_list.append(command_dict)
+	command_dict = { 'type' : 'sendline', 'command' : send_part }
+	commands_list.append(command_dict)
+
+def grab_config(destination_ip, commands_list, description):
+	sw_abort=0
+	composer="ssh " + account + "@" + destination_ip
+	child = pexpect.spawn(composer)
+	fp = open("fw_" + destination_ip + "_" + description + ".conf","w")
+
+	returnval = child.expect (["Are you sure you want to continue connecting","password",pexpect.EOF,pexpect.TIMEOUT])
+
+	if returnval == 0:
+		#RSA key message presented for SSH
+		child.sendline ("yes")
+		child.expect ("ssword:")
+		child.sendline (mypass+"\n")
+	elif returnval == 1:
+		#There is no RSA key presented to SSH
+		#child.expect ("ssword:")
+		child.sendline (mypass+"\n")
+	elif returnval == 2:
+		print ("\nEOF - Cannot connect to host " + destination_ip)
+		child.sendcontrol("c")
+		sw_abort=1
+	elif returnval == 3:
+		print ("\nTIMEOUT - Cannot connect to host " + destination_ip)
+		child.sendcontrol("c")
+		sw_abort=1
+
+	if sw_abort == 1:
+		print "aborted"
+
+	commands_size=len(commands_list)
+	#print "total " + str(commands_size) + " commands "
+
+	counter=0;
+	for dict in commands_list:
+  		kind=dict['type']
+		cmd=dict['command']
+		counter += 1
+		if kind == "expect":
+			child.expect(cmd)
+		elif kind == "sendline":
+			child.sendline(cmd)
+
+	fp.write(child.before)
+	fp.close()
+
+
+def main(get_config_ip):
+	global commands_list
+	commands_list=[]
+	add_expect_pairs("/act.*> ","en")
+	add_expect_pairs("word",mypass)
+	add_expect_pairs("/act.*# ","conf t")
+	add_expect_pairs("#","no pager")
+	add_expect_pairs("# ","exit")
+	# 
+	add_expect_pairs("/act.*# ","changeto system")
+	add_expect_pairs("/act.*# ","show run context")
+	add_expect_pairs("/act.*# ","exit")
+	print "getting ----------------------- " + get_config_ip + " main config "
+
+	grab_config(get_config_ip, commands_list, "main")
+
+	# open the main file an loop thru contexts
+	full_config_file=open(get_config_ip + "_main.conf","r").read()
+	lines_config_file=full_config_file.split('\n')
+
+	context_list=[]
+	for line in lines_config_file:
+		found_context=False
+
+		if re.match("^context ", line):
+			array_line=line.split(' ')
+			context_list.append(array_line[1].rstrip())
+			found_context=True
+
+	for contexto in context_list:
+			commands_list=[]
+			add_expect_pairs("/act.*>","en")
+			add_expect_pairs("word",mypass)
+			add_expect_pairs("/act.*# ","conf t")
+			add_expect_pairs("#","no pager")
+			add_expect_pairs("# ","exit")
+			# 
+			add_expect_pairs("/act.*# ","changeto context "+contexto)
+			add_expect_pairs("/act.*# ","show run")
+			add_expect_pairs("/act.*# ","exit")
+			print "getting ----------------------- " + get_config_ip + " context " + contexto
+			grab_config(get_config_ip, commands_list, contexto)
+
+
+# la la la la main stuff here
+firewallcitos=[ "192.168.1.1", 
+		"192.168.10.1",
+	        "192.168.10.2"]
+
+for firewallcito in firewallcitos:
+	main(firewallcito)
